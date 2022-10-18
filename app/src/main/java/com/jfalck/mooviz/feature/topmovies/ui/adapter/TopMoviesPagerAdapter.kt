6654c86ed294eca9a32fe7138df2dfad18a9dca6
jeremyfalck <jeremy.falck@proton.me>
2022-10-18 22:09:02 +0200

    added animation

--- app/src/main/java/com/jfalck/mooviz/feature/topmovies/ui/adapter/TopMoviesPagerAdapter.kt
+++ app/src/main/java/com/jfalck/mooviz/feature/topmovies/ui/adapter/TopMoviesPagerAdapter.kt
@@ -1,7 +1,10 @@
 package com.jfalck.mooviz.feature.topmovies.ui.adapter
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.ScaleAnimation
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -10,6 +13,8 @@ import com.jfalck.domain.model.Movie
 import com.jfalck.mooviz.databinding.TopMovieItemBinding
 import dagger.hilt.android.scopes.FragmentScoped
 import javax.inject.Inject
+import kotlin.random.Random
+
 
 @FragmentScoped
 class TopMoviesPagerAdapter @Inject constructor() :
@@ -20,6 +25,7 @@ class TopMoviesPagerAdapter @Inject constructor() :
 
     override fun onBindViewHolder(holder: TopMovieViewHolder, position: Int) {
         val item = getItem(position)
+        setAnimation(holder.itemView, position)
         holder.binding.topMovieItemTitle.text = item?.title
         Glide.with(holder.itemView.context).load(BASE_POSTER_URL + item?.posterPath)
             .into(holder.binding.topMoviePoster)
@@ -31,4 +37,24 @@ class TopMoviesPagerAdapter @Inject constructor() :
                 LayoutInflater.from(parent.context), parent, false
             )
         )
+
+    private var mLastPosition = -1
+
+    private fun setAnimation(viewToAnimate: View, position: Int) {
+        if (position > mLastPosition) {
+            val anim = ScaleAnimation(
+                0.0f,
+                1.0f,
+                0.0f,
+                1.0f,
+                Animation.RELATIVE_TO_SELF,
+                0.5f,
+                Animation.RELATIVE_TO_SELF,
+                0.5f
+            )
+            anim.duration = Random.nextLong(501) //to make duration random number between [0,501)
+            viewToAnimate.startAnimation(anim)
+            mLastPosition = position
+        }
+    }
 }
